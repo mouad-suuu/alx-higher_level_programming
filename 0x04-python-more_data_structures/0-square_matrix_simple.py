396f1518ac1737a154481272b448f455d950977e
mouad-suuu <moad.menniui@gmail.com>
2023-09-13 17:05:59 +0000

    task 0 fixing

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-import copy
-
 def square_matrix_simple(matrix=[]):
-    new_matrix = copy.deepcopy(matrix)
+    new_matrix = matrix.copy()
+
     for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            new_matrix[i][j] = matrix[i][j] ** 2
-    return new_matrix
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+
+    return (new_matrix)
