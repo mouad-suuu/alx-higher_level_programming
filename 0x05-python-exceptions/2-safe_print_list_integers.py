a215af32edf16feb3d4d23b36bed4a1a54883b61
mouad-suuu <moad.menniui@gmail.com>
2023-09-25 15:33:40 -0700

    task 2 fixing1.2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,8 +7,6 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         except (ValueError, TypeError):
             pass
-        except IndexError:
-            break
         i += 1
     print()
     return count
