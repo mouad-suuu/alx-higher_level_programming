e8dcfaa743e12d909d9556d634bf8ce679c927e1
mouad-suuu <moad.menniui@gmail.com>
2023-09-25 16:11:15 -0700

    task 100 fixing

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,11 +2,9 @@
 import sys
 
 def safe_print_integer_err(value):
-    is_int = True
     try:
         print("{:d}".format(value))
-        return True
+        return (True)
     except (ValueError, TypeError) as e:
         print("Exception:", e, file=sys.stderr)
-        is_int = False
-        return is_int
+        return (False)
