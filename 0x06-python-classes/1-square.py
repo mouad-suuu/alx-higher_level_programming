d90ef2428f1b0db6158be68bc7b96473e7a01413
mouad-suuu <moad.menniui@gmail.com>
2023-09-26 16:59:24 -0700

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""define a class square."""
+
 
 class Square:
+    """represent a square"""
 
     def __init__(self, size):
+        """initialize a new square.
+        
+        aregs:
+            size (int) : the size of the new square
+            """
         self.__size = size
         
