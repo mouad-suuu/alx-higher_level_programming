200faf1b65711faf17eb5352371bf2017c2ebfc2
mouad-suuu <moad.menniui@gmail.com>
2023-12-11 23:48:35 +0100

    firat task

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
-// 0-javascript_is_amazing.js
-
-// Create a constant variable called myVar
-const myVar = "JavaScript is amazing";
-
-// Use console.log(...) to print the output
+const myVar = 'JavaScript is amazing';
 console.log(myVar);
