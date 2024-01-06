d018fb037af7f5daa5cb6ff3c40c2ac0e68b680a
mouad-suuu <moad.menniui@gmail.com>
2024-01-05 18:49:19 -0800

    interview task

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-"""peak-finding algorithm."""
+"""Defines a peak-finding algorithm."""
+
 
 def find_peak(list_of_integers):
-    """Find the peak element in a list of integers."""
-    if not list_of_integers: return None
-    left, right = 0, len(list_of_integers) - 1
-    while left < right:
-        mid = (left + right) // 2
-        # If the middle is smaller than its adjacent elements, it can't be the peak.
-        if list_of_integers[mid] > list_of_integers[mid+1]:
-            right = mid 
-        else:
-            left = mid + 1
-    return list_of_integers[left]
+    """ Finds the peak in a list of integers """
+    if list_of_integers == []:
+        return None
 
+    length = len(list_of_integers)
+    m = int(length / 2)
+    li = list_of_integers
+
+    if m - 1 < 0 and m + 1 >= length:
+        return li[m]
+    elif m - 1 < 0:
+        return li[m] if li[m] > li[m + 1] else li[m + 1]
+    elif m + 1 >= length:
+        return li[m] if li[m] > li[m - 1] else li[m - 1]
+
+    if li[m - 1] < li[m] > li[m + 1]:
+        return li[m]
+
+    if li[m + 1] > li[m - 1]:
+        return find_peak(li[m:])
+    return find_peak(li[:m])
