2690cd0caaaec8a27a682e1cfe8c4e1876874d86
mouad-suuu <moad.menniui@gmail.com>
2024-01-08 01:21:55 +0100

    task 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""
- a Python script that fetches https://alx-intranet.hbtn.io/status
+"""fetches https://intranet.hbtn.io/status"""
+from urllib import request
 
-"""
-
-if __name__ == '__main__':
-    import urllib.request
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
-        content = resp.read()
-        print("Body response:$")
-        print("\t- type: {}$".format(type(content)))
-        print("\t- content: {}$".format(content))
-        print("\t- utf8 content: {}$" .format(content.decode('utf-8')))
-        
+if __name__ == "__main__":
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
+        response = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
