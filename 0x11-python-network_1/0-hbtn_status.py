8f85b4916d048c861a78887f00b5cd219a9e75c7
mouad-suuu <moad.menniui@gmail.com>
2024-01-08 01:31:39 +0100

    trying last check

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,8 +7,8 @@
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
-        content = res.read()
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as reso:
+        content = reso.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,21 +1,10 @@
 #!/usr/bin/python3
 """
-A Python script that takes in a URL, sends a request to the URL, and
-displays the value of the X-Request-Id variable found in the header of the response.
+Take in a URL, send request to URL and display value of `X-Request-Id`
 """
+import sys
+from urllib import request
 
-if __name__ == '__main__':
-    import urllib.request
-    import sys
-
-    # Check if a URL is provided
-    if len(sys.argv) != 2:
-        print("Usage: {} <URL>".format(sys.argv[0]))
-        sys.exit(1)
-
-    url = sys.argv[1]
-
-    # Send a request to the URL and display the value of X-Request-Id
-    with urllib.request.urlopen(url) as response:
-        x_request_id = response.headers.get('X-Request-Id')
-        print(x_request_id)
+if __name__ == "__main__":
+    with request.urlopen(sys.argv[1]) as res:
+        print(res.info()['X-Request-Id'])
